e5ae2548da51fe1528aaf23ef50ec9fb62a23495
calibraintinfo-maker <calibraintinfo@gmail.com>
2025-09-04 22:08:45 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,35 +21,20 @@ function App() {
   return (
     <AuthProvider>
       <Router>
-        <div className="app-wrapper">
+        <div>
           <Navbar />
-          <main className="main-content">
+          <main style={{ paddingTop: '80px', minHeight: 'calc(100vh - 80px)' }}>
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/find-property" element={<FindProperty />} />
               <Route path="/property/:id" element={<PropertyDetails />} />
-              <Route 
-                path="/book/:propertyId" 
-                element={<ProtectedRoute><BookProperty /></ProtectedRoute>} 
-              />
-              <Route 
-                path="/my-bookings" 
-                element={<ProtectedRoute><MyBookings /></ProtectedRoute>} 
-              />
-              <Route 
-                path="/add-property" 
-                element={<ProtectedRoute><AddProperty /></ProtectedRoute>} 
-              />
-              <Route 
-                path="/manage-properties" 
-                element={<ProtectedRoute><ManageProperties /></ProtectedRoute>} 
-              />
-              <Route 
-                path="/profile" 
-                element={<ProtectedRoute><Profile /></ProtectedRoute>} 
-              />
+              <Route path="/book/:propertyId" element={<ProtectedRoute><BookProperty /></ProtectedRoute>} />
+              <Route path="/my-bookings" element={<ProtectedRoute><MyBookings /></ProtectedRoute>} />
+              <Route path="/add-property" element={<ProtectedRoute><AddProperty /></ProtectedRoute>} />
+              <Route path="/manage-properties" element={<ProtectedRoute><ManageProperties /></ProtectedRoute>} />
+              <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
             </Routes>
           </main>
           <Footer />
