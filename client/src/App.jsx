1ab3a73c3af7eac8bcbca29b39dd53a16e1a30ac
calibraintinfo-maker <calibraintinfo@gmail.com>
2025-09-04 22:18:33 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,37 +21,22 @@ function App() {
   return (
     <AuthProvider>
       <Router>
-        <div className="app-container">
+        <div>
           <Navbar />
-          <main className="main-content">
+          <div style={{ paddingTop: '80px' }}>
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/find-property" element={<FindProperty />} />
               <Route path="/property/:id" element={<PropertyDetails />} />
-              <Route 
-                path="/book/:propertyId" 
-                element={<ProtectedRoute><BookProperty /></ProtectedRoute>} 
-              />
-              <Route 
-                path="/my-bookings" 
-                element={<ProtectedRoute><MyBookings /></ProtectedRoute>} 
-              />
-              <Route 
-                path="/add-property" 
-                element={<ProtectedRoute><AddProperty /></ProtectedRoute>} 
-              />
-              <Route 
-                path="/manage-properties" 
-                element={<ProtectedRoute><ManageProperties /></ProtectedRoute>} 
-              />
-              <Route 
-                path="/profile" 
-                element={<ProtectedRoute><Profile /></ProtectedRoute>} 
-              />
+              <Route path="/book/:propertyId" element={<ProtectedRoute><BookProperty /></ProtectedRoute>} />
+              <Route path="/my-bookings" element={<ProtectedRoute><MyBookings /></ProtectedRoute>} />
+              <Route path="/add-property" element={<ProtectedRoute><AddProperty /></ProtectedRoute>} />
+              <Route path="/manage-properties" element={<ProtectedRoute><ManageProperties /></ProtectedRoute>} />
+              <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
             </Routes>
-          </main>
+          </div>
           <Footer />
         </div>
       </Router>
